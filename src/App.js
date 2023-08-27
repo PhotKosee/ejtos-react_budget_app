1d45014e187bf03212ce6645899e8ca25df51995
Phot Koseekrainiramon <z5387411@ad.unsw.edu.au>
2023-08-28 00:00:50 +1000

    hook budget

--- src/App.js
+++ src/App.js
@@ -6,20 +6,28 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
-
 import { AppProvider } from './context/AppContext';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {/* Add Budget component here under */}        
+                    
+                        <div className='col-sm'>
+                            <Budget />
+                        </div>
 
-                        {/* Add Remaining component here under */}        
+                        <div className='col-sm'>
+                            <Remaining />    
+                        </div>        
 
-                        {/* Add ExpenseTotal component here under */}        
+                        <div className='col-sm'>
+                            <ExpenseTotal />
+                        </div>        
                        
                         {/* Add ExpenseList component here under */}         
 
